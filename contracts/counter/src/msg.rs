4bba0e97763779ac7d747f58b9089fa1b63ac956
BiPhan4 <bi.phan@ryerson.ca>
2022-03-26 17:41:00 -0400

    new changes

--- contracts/counter/src/msg.rs
+++ contracts/counter/src/msg.rs
@@ -3,14 +3,16 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    pub count: i32, //what do we initialize? 
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     Increment {},
-    Reset { count: i32 },
+    Reset { count: i32 }, 
+    //we will get user address and names. Then divide bill. Then take money from users and put into our own contract.
+    //
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
--- contracts/counter/src/state.rs
+++ contracts/counter/src/state.rs
@@ -2,7 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::Item; //import map and figure out how to use it 
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
@@ -11,3 +11,14 @@ pub struct State {
 }
 
 pub const STATE: Item<State> = Item::new("state");
+
+/*
+add states:
+
+1 - the bill(s) / item 
+2 - the customers /map 
+3 - Need to figure out a way to store money that we've taken from users. Third state is item again? 
+
+1 - how to use map and item to store customers or any state
+2 - how to display bill to user 
+*/
