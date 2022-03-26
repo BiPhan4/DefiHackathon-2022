287f2d6c7f44c8fb6aecf02c22708e7828de85eb
alex324998301 <102430172+alex324998301@users.noreply.github.com>
2022-03-26 19:50:20 -0400

    payup method work-in progress
    buisness is the owner of the contract--> requests the amount on the bill--> the customer interacts with the same contract to pay the bill

--- keys.terrain.js
+++ keys.terrain.js
@@ -2,11 +2,8 @@
 // to populate secret in CI environment instead of hardcoding
 
 module.exports = {
-  custom_tester_1: {
+  testnet: {
     mnemonic:
-      "shiver position copy catalog upset verify cheap library enjoy extend second peasant basic kit polar business document shrug pass chuckle lottery blind ecology stand",
-  },
-  custom_tester_2: {
-    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+      "rose affair garlic decorate awesome round upgrade distance novel strong this congress jelly title also trigger proof stand between humble primary short area people",
   },
 };
